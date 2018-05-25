146d276dde7e7df9200153b6608708e7ffe47e4c
xiaoting <xiaot@toolink.cn>
2018-05-25 10:39:17 +0800

    。。。

--- src/components/rank/rank.vue
+++ src/components/rank/rank.vue
@@ -1,9 +1,26 @@
 <template>
-	<div>排行页面</div>
+	<div>
+        <canvas id="canvas" ref="canvas" width="340" height="500" style="border: 1px solid #dedede;margin:10px;display: block">
+
+        </canvas>
+    </div>
 </template>
 
-<script>
+<script type="text/javascript">
+export default {
+    data(){
+        return{
+        }
+    },
+    methods:{
+
+    },
+}
+
 </script>
 
-<style>
+<style lang="scss" scoped>
+    #canvas{
+        margin: 0 auto;
+    }
 </style>
